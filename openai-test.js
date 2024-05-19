fdb4f1fc6e844b9a8fd78490237ec850db4d8377
Stephen Handy <bolds-slicks0g@icloud.com>
2024-05-19 12:51:08 -0400

    remove quotes

--- openai-test.js
+++ openai-test.js
@@ -8,11 +8,18 @@ const openai = new OpenAI({
 console.log(openai);
 
 async function test() {
-    const image = await openai.images.generate({
-        prompt: "A cute baby sea otter"
-    })
-
-    console.log(image.data);
+    try {
+        const image = await openai.images.generate({
+            prompt: "A cute baby sea otter",
+            n: 1,
+            size: '512x512'
+        })
+    
+        console.log(image.data);
+    } catch (error) {
+        console.error('Error generating image', error);
+    }
+    
 }
 
 test();
